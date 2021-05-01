abfc29c65e07b7f6d0fb04444f46fb3e6a3ab190
Vinicius José <vinicius.jsilv@gmail.com>
2021-05-01 20:10:28 -0300

    Finalizaçao do layout da tela de login

--- login.php
+++ login.php
@@ -11,9 +11,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-    <!-- Bootstrap CSS -->
-    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel="stylesheet" href="style/style.css">
     <!-- Font Awesome -->
     <script src="https://kit.fontawesome.com/bf7e05c402.js" crossorigin="anonymous"></script>
@@ -36,12 +35,12 @@
                     <div class="mb-3 input-group">
                         <span class="input-group-addon icon-login"><i class="fas fa-envelope"></i></span>
                         <input type="email" name= "email" required class="form-control input-icon" id="exampleInputEmail1" placeholder = "Digite seu e-mail" aria-describedby="emailHelp">
-                        <div id="emailHelp" class="form-text">Nós jamais iremos compartilhar seus dados com outras pessoas.</div>
                     </div>
                     <div class="mb-3 input-group">
                         
                         <span class="input-group-addon icon-senha"><i class="fas fa-lock"></i></span>
                         <input type="password" name= "senha" required class="form-control input-icon" id="exampleInputPassword1" placeholder = "Digite sua senha">
+                        <div id="emailHelp" class="form-text">Todos os seus dados estarão armazenados em segurança conosco.</div>
                     </div>
                     <?php
                         $usuario = "";
@@ -56,9 +55,13 @@
                             $sql->execute();
                             if($sql->rowCount() > 0){
                                 $usuario = $sql->fetch();
+                                if(isset($_POST['manter_login']) && !empty($_POST['manter_login'])){
+                                    $_SESSION['id_usuario'] = $usuario['id_usuario'];
+                                }
                                 header("Location: index.php");
                                 exit;
                             }else{
+                                
                                 echo "<p class = 'invalid-user'>Usuário e senha inválidos</p>";
                             }
                         }
@@ -75,7 +78,9 @@
         </div>
         
                 
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+         
+        <script src="https://code.jquery.com/jquery-3.6.0.slim.js" integrity="sha256-HwWONEZrpuoh951cQD1ov2HUK5zA5DwJ1DNUXaM6FsY=" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
   </body>
 </html>
