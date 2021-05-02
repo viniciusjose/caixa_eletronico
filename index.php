0b509defe44ec6f27988a87aac663bd25a3bd4b4
Vinicius José <vinicius.jsilv@gmail.com>
2021-05-01 22:45:53 -0300

    Preparaçao do layout da pagina index, adicionado nome do usuario dinamicamente ao menu dropdown da pagina inicial

--- index.php
+++ index.php
@@ -6,13 +6,13 @@
     header("Location: login.php");
     exit;
   }
-
-  $sql = "SELECT id_usuario FROM usuarios WHERE id_usuario = :id";
+  $usuario = "";
+  $sql = "SELECT * FROM usuarios WHERE id_usuario = :id";
   $sql = $database->prepare($sql);
   $sql->bindValue(":id", $_SESSION['id_usuario']);
   $sql->execute();
   if($sql->rowCount() > 0){
-    
+    $usuario = $sql->fetch();
   }else{
     header("Location login.php");
     exit;
@@ -30,14 +30,30 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel="stylesheet" href="style/style.css">
+    <script src="https://kit.fontawesome.com/bf7e05c402.js" crossorigin="anonymous"></script>
     <link rel="shortcut icon" href="img/fav-icon.jpg" />
     <title>Santander - Página Inicial</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
-
-
-    
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark fixed-top navbar-customize">
+      <a class="navbar-brand" href="#">
+        <img src="img/logo-santander.png" width="200" class="d-inline-block align-top" alt="">
+      </a>
+      <div class="collapse navbar-collapse" id="navbarNav">
+      </div>
+      <ul class="navbar-nav user-dropdown">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-user icon-usuario"></i><?php echo $usuario['nome']?>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="alterar.php">Alterar informações</a>
+            <a class="dropdown-item" href="sair.php">Logout</a>
+          </div>
+        </li>
+      </ul>
+        
+    </nav>    
 
     <script src="https://code.jquery.com/jquery-3.6.0.slim.js" integrity="sha256-HwWONEZrpuoh951cQD1ov2HUK5zA5DwJ1DNUXaM6FsY=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
